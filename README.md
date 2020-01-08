27e407f27b3ee4332d415f80bd9280494f9c6193
nidhinmahesh <nidhinmahesh1@gmail.com>
2020-01-08 15:42:23 +0530

    initial build

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
 # smart_dispenser_wallet
+## Following are the proposed functions for the application -
+- [ ] Read IOTA QR,
+- [ ] Connect to network,
+- [ ] Join to IOTA,
+- [ ] Make payment,
+- [ ] Choose requirements
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+// *****************************************************************************
+// * | File        :	  main.dart
+// * | Author      :   Nidhin Mahesh A
+// * | Function    :   Launch screen
+// * | Info        :
+// *----------------
+// * | This version:   V1.0.0
+// * | Date        :   2020-01-05
+// # | Info        :   This is the launch screen that runs on app launch to check
+//                     login status of the user to the app.
+// -----------------------------------------------------------------------------
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documnetation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to  whom the Software is
+// furished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Coolsunday',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        accentColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+        textSelectionHandleColor: Colors.black,
+        textSelectionColor: Colors.black12,
+        cursorColor: Colors.black,
+        toggleableActiveColor: Colors.black,
+
+        inputDecorationTheme: InputDecorationTheme(
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black.withOpacity(0.1)),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+          ),
+          labelStyle: const TextStyle(
+            color: Colors.black,
+          ),
+        ),
+
+        fontFamily: 'Noto Sans',
+        primarySwatch: Colors.blue,
+      ),
+      home: Launch(),
+    );
+  }
+}
+
